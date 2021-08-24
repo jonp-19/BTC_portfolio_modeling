1af078d783672eb97da69379f3d3ecff76a912c7
jonp-19 <jonpokorney@gmail.com>
2021-08-24 10:10:15 -0500

    Changed to class

--- btc_prices_pandas.py
+++ btc_prices_pandas.py
@@ -6,82 +6,84 @@ import matplotlib.pyplot as plt
 from pandas.core.frame import DataFrame
 
 #TODO docstring needs work.  Formatting is wrong.
-def model_btc_portfolio(
-    start_date: datetime, end_date: datetime, initial_invest: int, bank: int, high_mark: int, low_mark: int
-    ) -> DataFrame:
-    """Inputs:
-            start_date must be "YYYY-MM-DD"
-            end_date must be "YYYY_MM_DD"
-            initial_invest is the funds used for initial investment
-            bank is amount of funds set aside for additional purchases
-            high mark is the highest portfolio value will go before selling
-            low mark is the lowest portfolio value will go before buying
-        Returns:
-            dataframe with dates, close_prices, current_holdings_list, shares, balance
-            final total holdings amount
-        """
-    # create a dataframe of BTC price information
-    df = pd.read_csv('data/BTC-USD.csv', parse_dates=['Date'])
+class BTCModel(object):
+    """Class for modeling portfolio balances of BTC"""
+    def __init__(self, start_date: datetime, end_date: datetime, initial_invest: int, bank: int, high_mark: int, low_mark: int):
+        self.start_date = start_date
+        self.end_date = end_date
+        self.initial_invest = initial_invest
+        self.bank = bank
+        self.high_mark = high_mark
+        self.low_mark = low_mark
+        
+        # create a dataframe of BTC price information
+        self.df = pd.read_csv('data/BTC-USD.csv', parse_dates=['Date'])
 
-    # create dataframe limited to between dates
-    dated_frame = df[df.Date.between(start_date, end_date)]
+        # create dataframe limited to between dates
+        self.dated_frame = self.df[self.df.Date.between(self.start_date, self.end_date)]
 
-    # create date and close price series
-    close_prices = (dated_frame.loc[:, 'Close'])
-    dates = (dated_frame.loc[:, 'Date'])
+        # create date and close price series
+        self.close_prices = (self.dated_frame.loc[:, 'Close'])
+        self.dates = (self.dated_frame.loc[:, 'Date'])
 
-    # set initial lists
-    shares, balance, current_holdings_list = [], [], []
+        # set initial lists
+        self.shares, self.balance, self.current_holdings_list = [], [], []
 
+    # Get current holdings
+    def getCurrentHoldings(self, shares, y):
+            self.current_holdings = self.shares[-1] * y
+            self.current_holdings_list.append(float(self.current_holdings))
+            return self.current_holdings
+    
+    # Make initial purchase.  Trying without arguments.
+    def makeInitialPurchase(self):
+        self.initial_purchase = float(self.initial_invest/self.close_prices[:1])
+        self.shares.append(self.initial_purchase)
+        self.current_holdings = self.shares[-1] * self.y
+        self.current_holdings_list.append(float(self.current_holdings))
+        self.balance.append(self.bank)
+
+    def holdBTC(self):
+        self.shares.append(self.shares[-1])
+        self.balance.append(self.balance[-1])
+
+    def sellBTC(self):
+        self.sell_btc = (self.current_holdings - self.high_mark)/self.y
+        self.realized_profit = self.sell_btc * self.y
+        self.shares.append(self.shares[-1] - self.sell_btc)
+        self.balance.append((self.balance[-1]) + (self.realized_profit))
+
+    def buyBTC(self):
+        self.buy_btc = (self.low_mark - self.current_holdings)/self.y
+        self.spent_now = self.buy_btc * self.y
+        self.shares.append(self.shares[-1] + self.buy_btc)
+        self.balance.append((self.balance[-1]) - (self.spent_now))
+    
     # Run though dates that were set earlier.  Purchase, set, or hold based on 
     # value of portfolio.  Try/except sets up initial purchase.
-    for x,y in zip(dates, close_prices):
-        try:
-            current_holdings = shares[-1] * y
-            current_holdings_list.append(float(current_holdings))
-        except IndexError:
-            initial_purchase = float(initial_invest/close_prices[:1])
-            shares.append(initial_purchase)
-            current_holdings = shares[-1] * y
-            current_holdings_list.append(float(current_holdings))
-            balance.append(bank)
-        else:
-            if current_holdings > high_mark:
-                sell_btc = (current_holdings - high_mark)/y
-                realized_profit = sell_btc * y
-                shares.append(shares[-1] - sell_btc)
-                balance.append((balance[-1]) + (realized_profit))
-            elif current_holdings < low_mark and balance[-1] > 0:
-                buy_btc = (low_mark - current_holdings)/y
-                spent_now = buy_btc * y
-                shares.append(shares[-1] + buy_btc)
-                balance.append((balance[-1]) - (spent_now))
+    def createBalance(self):
+        for self.x, self.y in zip(self.dates, self.close_prices):
+            try:
+                self.getCurrentHoldings(self.shares, self.y)
+            except IndexError:
+                self.makeInitialPurchase()
             else:
-                shares.append(shares[-1])
-                balance.append(balance[-1])
+                if self.current_holdings > self.high_mark:
+                    self.sellBTC()
+                elif self.current_holdings < self.low_mark and self.balance[-1] > 0:
+                    self.buyBTC()
+                else:
+                    self.holdBTC()
 
-    new_df = pd.DataFrame(list(zip(dates, close_prices, current_holdings_list, shares, balance)))
-    print(new_df)
+        self.getTotalBalance(self.dates, self.close_prices, self.current_holdings_list, self.shares, self.balance)
 
-    total_balance = list((a + b for a,b in zip(current_holdings_list, balance)))
-    print(total_balance[-1])
-    plot_balance(dates, total_balance)
+    def getTotalBalance(self, dates, close_prices, current_holdings_list, shares, balance):
+        self.new_df = pd.DataFrame(list(zip(self.dates, self.close_prices, self.current_holdings_list, self.shares, self.balance)))
+        print(self.new_df)
+        self.new_df.to_csv(f'{self}BTC_growth.csv')
 
-def plot_balance(dates, total_balance):
-    # Plot the BTC close prices
-    plt.style.use('seaborn')
-    fig, ax = plt.subplots()
-    ax.plot(dates, total_balance, c='red', alpha=0.5)
+        self.total_balance = list((a + b for a,b in zip(self.current_holdings_list, self.balance)))
+        print(self.total_balance[-1])
 
-    # Format plot.
-    ax.set_title(f"BTC Balance Retention", fontsize=24)
-    ax.set_xlabel('', fontsize=16)
-    fig.autofmt_xdate()
-    ax.set_ylabel("USD", fontsize=16)
-    ax.tick_params(axis='both', which='major', labelsize=16)
-
-    plt.show()
-
-model_btc_portfolio("2015-04-24", "2021-04-28", 5000, 5000, 5200, 5000)
-
+#model_btc_portfolio("2015-04-24", "2021-04-28", 5000, 5000, 5200, 5000)
 # %%
